7b0e86b9199675a6b4eb3fcff27d8720cc72f67d
cklwblove <lwbhtml@163.com>
2017-08-17 16:50:50 +0800

    删除插件

--- template/src/pages/login/page.js
+++ template/src/pages/login/page.js
@@ -1,13 +1,7 @@
 require('cp');
-require('@plugins/iCheck/minimal/_all.css');
-require('@plugins/iCheck/icheck.min');
 require('./page.less');
 
 $(function () {
-  $('input').iCheck({
-    checkboxClass: 'icheckbox_minimal-blue',
-    radioClass: 'iradio_minimal-blue',
-    increaseArea: '20%' // optional
-  });
+  
 })
 
